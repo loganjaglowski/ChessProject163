cbfe65413da1e1e96a0bba05cf4bb40fc3181133
loganjaglowski <30781051+loganjaglowski@users.noreply.github.com>
2019-03-18 19:58:41 -0400

    Update Knight.java

--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -12,10 +12,131 @@ public class Knight extends ChessPiece {
 
     public boolean isValidMove(Move move, IChessPiece[][] board){
 
-        boolean valid = true;
+        boolean valid = false;
         // More code is needed
+        if (super.isValidMove(move, board)) {
+            if (move.fromRow == move.toRow - 2)
+                if (move.fromColumn == move.toColumn - 1) {
+                    if (super.player() == Player.WHITE) {
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                    if (super.player() == Player.BLACK){
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                }
+            if (move.fromRow == move.toRow + 2)
+                if (move.fromColumn == move.toColumn - 1) {
+                    if (super.player() == Player.WHITE) {
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                    if (super.player() == Player.BLACK){
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                }
+            if (move.fromRow == move.toRow + 2)
+                if (move.fromColumn == move.toColumn + 1) {
+                    if (super.player() == Player.WHITE) {
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                    if (super.player() == Player.BLACK){
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                }
+            if (move.fromRow == move.toRow - 2)
+                if (move.fromColumn == move.toColumn + 1) {
+                    if (super.player() == Player.WHITE) {
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                    if (super.player() == Player.BLACK){
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                }
+            if (move.fromRow == move.toRow - 1)
+                if (move.fromColumn == move.toColumn - 2) {
+                    if (super.player() == Player.WHITE) {
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                    if (super.player() == Player.BLACK){
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                }
+            if (move.fromRow == move.toRow - 1)
+                if (move.fromColumn == move.toColumn + 2) {
+                    if (super.player() == Player.WHITE) {
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                    if (super.player() == Player.BLACK){
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                }
+            if (move.fromRow == move.toRow + 1)
+                if (move.fromColumn == move.toColumn - 2) {
+                    if (super.player() == Player.WHITE) {
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                    if (super.player() == Player.BLACK){
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                }
+            if (move.fromRow == move.toRow + 1)
+                if (move.fromColumn == move.toColumn + 2) {
+                    if (super.player() == Player.WHITE) {
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                    if (super.player() == Player.BLACK){
+                        if (board[move.toRow][move.toColumn] == null)
+                            return true;
+                        if (board[move.toRow][move.toColumn].player() == Player.BLACK)
+                            valid = true;
+                    }
+                }
+        }
         return valid;
-
     }
 
 }
