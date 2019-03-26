b95d3959580da3012417e1ccbb784ca334e0f9b3
loganjaglowski <30781051+loganjaglowski@users.noreply.github.com>
2019-03-26 03:38:21 -0400

    Update Bishop.java

--- src/chess/Bishop.java
+++ src/chess/Bishop.java
@@ -1,5 +1,11 @@
 package chess;
 
+/**********************************************************************
+ * A class that determines the correct Bishop values.
+ *
+ * @author Logan Jaglowski, Sarah, and Lauren
+ * @version Winter 2019
+ *********************************************************************/
 public class Bishop extends ChessPiece {
 
     /*****************************************************************
